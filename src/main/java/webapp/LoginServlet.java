69bf33ca67c3e1a528593472eef2a54f165ac42b
Sohan Jain <snsohanjain@gmail.com>
2023-03-21 17:28:02 +0530

    first commit

---
+++ src/main/java/webapp/LoginServlet.java
@@ -0,0 +1,27 @@
+package webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Browser sends Http Request to Web Server
+ * 
+ * Code in Web Server => Input:HttpRequest, Output: HttpResponse
+ * JEE with Servlets
+ * 
+ * Web Server responds with Http Response
+ */
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request,response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Yahoo!!!!!!!!</title>
+  </head>
+  <body>
+    <h1>My First JSP</h1>
+  </body>
+</html>
