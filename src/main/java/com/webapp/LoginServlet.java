425a2ca95b314f47d3def0c33a5cee21b0c87caa
Sohan Jain S N <snsohanjain@gmail.com>
2023-03-21 23:28:58 +0530

    User Login

--- src/main/java/com/webapp/LoginServlet.java
+++ src/main/java/com/webapp/LoginServlet.java
@@ -11,11 +11,26 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
+	private  UserValidationService service = new UserValidationService();
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		request.setAttribute("name", request.getParameter("name"));
-		request.setAttribute("password",request.getParameter("password"));
 		request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request, response);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		boolean isUserValid = service.isUserValid(name,password);
+		if(isUserValid){
+		request.setAttribute("name", name);
+		request.setAttribute("password", password);
+		request.getRequestDispatcher("WEB-INF/views/welcome.jsp").forward(request, response);
+		}else {
+			request.setAttribute("errorMessage","Invalid Credentials!");
+			request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request, response);
+		}
 	}
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -3,6 +3,9 @@
   <title>Yahoo!!</title>
 </head>
 <body>
-My First JSP!!! My name is ${name} and My Password is ${password}
+<form action="./login.do" method="post">
+  <p><font color="red">${errorMessage}</font></p>
+  Enter Your name <input type="text" name="name"/> Password <input type="password" name="password"> <input type="submit" value="login"/>
+</form>
 </body>
 </html>
