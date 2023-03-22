0f0706311dd7d33b4342e14a3bec511b76ffdb12
Sohan Jain <snsohanjain@gmail.com>
2023-03-22 14:31:10 +0530

    Step 7 AutoWiring and dependancy injuction

--- src/main/java/com/sohan/springmvc/LoginController.java
+++ src/main/java/com/sohan/springmvc/LoginController.java
@@ -1,16 +1,19 @@
 package com.sohan.springmvc;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class LoginController {
-    LoginService service = new LoginService();
+    private final LoginService service;
+    @Autowired
+    public LoginController(LoginService service) {
+        this.service = service;
+    }
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String showLoginPage(){
         return "login";
@@ -27,6 +30,7 @@ public class LoginController {
             model.put("name", name);
             return "welcome";
         }
+
     }
 
 }
--- src/main/java/com/sohan/springmvc/LoginService.java
+++ src/main/java/com/sohan/springmvc/LoginService.java
@@ -1,5 +1,8 @@
 package com.sohan.springmvc;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
     public boolean isUserValid(String user, String password){
         if(user.equals("Sohan Jain") && password.equals("12345")){
