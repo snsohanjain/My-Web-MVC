61740aec31c5626162fad772c75241849466b107
Sohan Jain <snsohanjain@gmail.com>
2023-03-23 18:49:01 +0530

    MODEL

--- src/main/java/com/sohan/todo/TodoController.java
+++ src/main/java/com/sohan/todo/TodoController.java
@@ -3,11 +3,13 @@ package com.sohan.todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.validation.Valid;
 import java.util.Date;
 
 @Controller
@@ -24,15 +26,23 @@ public class TodoController {
         return "list-todo";
     }
     @RequestMapping(value = "/add-todo", method = RequestMethod.GET)
-    public String showTodoPage(){
+    public String showAddTodoPage(ModelMap model) {
+        model.addAttribute("todo", new Todo());
         return "todo";
     }
     @RequestMapping(value = "/add-todo", method = RequestMethod.POST)
-    public String addTodo(ModelMap model , @RequestParam String desc){
-        service.addTodo("SohanJain", desc, new Date(),false);
-        model.clear();
+    public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+
+        if (result.hasErrors())
+            return "todo";
+
+        System.out.println(todo.getDesc());
+
+        service.addTodo("SohanJain",todo.getDesc(), new Date(), false);
+        model.clear();// to prevent request parameter "name" to be passed
         return "redirect:/list-todo";
     }
+
     @RequestMapping(value = "/delete-todo", method = RequestMethod.GET)
     public String deleteTodo(ModelMap model, @RequestParam int id){
         service.deleteTodo(id);
--- src/main/webapp/WEB-INF/views/todo.jsp
+++ src/main/webapp/WEB-INF/views/todo.jsp
@@ -6,15 +6,25 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <html>
 <head>
     <title>Todo</title>
 </head>
 <body>
-<h1>ADD TODO</h1>
-<form action="/add-todo" METHOD="post">
-    <input name="desc"  required="required" type="text"/>
-    <input type="submit" value="Add">
-</form>
+<div class="container">
+
+    <form:form method="post" commandName="todo">
+        <fieldset class="form-group">
+            <form:label path="desc">Description</form:label>
+            <form:input path="desc" type="text" class="form-control"
+                        required="required"/>
+            <form:errors path="desc" cssClass="text-warning" />
+
+        </fieldset>
+        <button type="submit" class="btn btn-success">Add</button>
+    </form:form>
+</div>
+
 </body>
 </html>
